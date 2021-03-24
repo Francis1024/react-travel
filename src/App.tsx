23c23cada08f20ad01408a76f4ca4f9df159a9b9
Francis <yiyboy@qq.com>
2021-03-24 11:08:06 +0800

    feat：✅ 首页轮播图。

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './App.module.css';
-import { Header, Footer,SideMenu } from './components';
-import {Row,Col,Typography} from 'antd'
+import { Header, Footer,SideMenu,Carousel } from './components';
+import {Row,Col} from 'antd'
 // import sideImage from './assets/images/sider_2019_12-09.png';
 // import sideImage2 from './assets/images/sider_2019_02-04.png';
 // import sideImage3 from './assets/images/sider_2019_02-04-2.png';
@@ -15,6 +15,9 @@ const App: React.FC = () => {
         <Row style={{marginTop:20}}>
           <Col span={6}>
             <SideMenu />
+          </Col>
+          <Col span={18}>
+            <Carousel />
           </Col>
         </Row>
       </div>
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 export * from './footer/footer';
 export * from './header/header';
 export * from './sideMenu';
+export * from './carousel';
